9b2407976ec27046397b1877942988add04db9c8
Tyler Wade <vaxonreal@gmail.com>
2025-05-14 20:53:05 -0400

    add Spring Doc configuration and dependencies for API documentation

--- backend/src/main/java/net/tylerwade/registrationsystem/config/security/SecurityConfig.java
+++ backend/src/main/java/net/tylerwade/registrationsystem/config/security/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(jwtCookieToAuthorizationFilter, BearerTokenAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("v3/api-docs/**", "/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/api/auth/signup").permitAll()
                         .requestMatchers("/api/admin/**").hasRole(UserRole.ADMIN.name())
                         .anyRequest().authenticated()
