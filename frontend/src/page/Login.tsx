96ab7329d2a6789960a4bef3beed99dea0cd0884
xraze <liljetski@gmail.com>
2025-05-29 12:20:16 -0500

    Add frontend page components and fix casing issues

---
+++ frontend/src/page/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login: React.FC = () => {
+    return (
+        <div className="text-3xl text-blue-600 p-4">
+            Login Page
+        </div>
+    );
+};
+
+export default Login;
