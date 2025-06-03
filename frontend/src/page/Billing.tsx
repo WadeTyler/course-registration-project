782c32071ed75e25029804b5b448557887b95b43
xraze <liljetski@gmail.com>
2025-06-03 02:11:53 -0500

    Update dashboard: Add grid layout, add grid cards, add dashboard functionality

---
+++ frontend/src/page/Billing.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Billing: React.FC = () => {
+    return (
+        <div className="text-3xl text-green-300 p-4">
+            Billing & Payments
+        </div>
+    );
+};
+
+export default Billing;
