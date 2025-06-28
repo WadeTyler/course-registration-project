64166c860b3a261f4ffbb8c7301f776c13ccd4ad
Tyler Wade <vaxonreal@gmail.com>
2025-06-28 12:25:38 -0400

    Refactor DefaultAccountsInitializer to always create default accounts
    Signed-off-by: Tyler Wade <vaxonreal@gmail.com>

--- backend/src/main/java/net/tylerwade/registrationsystem/auth/DefaultAccountsInitializer.java
+++ backend/src/main/java/net/tylerwade/registrationsystem/auth/DefaultAccountsInitializer.java
@@ -1,25 +1,20 @@
 package net.tylerwade.registrationsystem.auth;
 
-import net.tylerwade.registrationsystem.config.AppProperties;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DefaultAccountsInitializer implements CommandLineRunner {
     private final UserService userService;
-    private final AppProperties appProperties;
 
-    public DefaultAccountsInitializer(UserService userService, AppProperties appProperties) {
+    public DefaultAccountsInitializer(UserService userService) {
         this.userService = userService;
-        this.appProperties = appProperties;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        if (!appProperties.isProduction()) {
-            userService.createDefaultAdmin();
-            userService.createDefaultInstructor();
-            userService.createDefaultStudent();
-        }
+        userService.createDefaultAdmin();
+        userService.createDefaultInstructor();
+        userService.createDefaultStudent();
     }
 }
